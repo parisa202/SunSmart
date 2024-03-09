40b669b107958820de976ba6300883202c0d5dec
Parisa Tabrizi <ptab0003@student.monash.edu>
2024-03-09 23:27:17 +1100

    index page

--- Apps/CoreApp/views.py
+++ Apps/CoreApp/views.py
@@ -4,4 +4,4 @@ from django.core.mail import send_mail
 from django.core.management.utils import get_random_secret_key
 
 class HomeView(generic.TemplateView):
-    template_name = 'CoreApp/base.html'
+    template_name = 'CoreApp/test.html'
