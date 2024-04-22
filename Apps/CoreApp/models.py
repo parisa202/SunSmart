e194df16851f780dde3b3eb519c5d2ae481add64
Parisa Tabrizi <ptab0003@student.monash.edu>
2024-04-22 18:33:27 +1000

    add recipe tables

--- Apps/CoreApp/models.py
+++ Apps/CoreApp/models.py
@@ -25,3 +25,18 @@ class PB_WHO_BMI(models.Model):
     year = models.IntegerField( null=True, blank=True)
     gender = models.CharField(max_length=20, choices = GENDER_TYPE, null=True, blank=True)
     crude_estimate = models.FloatField(null=True, blank=True)
+    
+
+class HEALTH_TAG(models.Model):
+    tag = models.CharField(max_length=150, null=True, blank=True)
+
+class MEAL_TYPE(models.Model):
+    type = models.CharField(max_length=150, null=True, blank=True)
+    
+class RECIPES(models.Model):
+    name = models.CharField(max_length=150, null=True, blank=True)
+    description = models.TextField( null=True, blank=True)
+    health_tag = models.ManyToManyField(HEALTH_TAG, related_name='recipes_h')
+    meal = models.ManyToManyField(MEAL_TYPE, related_name='recipes_m')
+
+
--- SunSmart/urls.py
+++ SunSmart/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('Apps.CoreApp.urls')),
     path("user/", include('Apps.UserAuthentication.urls')),
-    path("v1/", include('Apps.V1CoreApp.urls')),
+    path("iteration1/", include('Apps.V1CoreApp.urls')),
 ]
